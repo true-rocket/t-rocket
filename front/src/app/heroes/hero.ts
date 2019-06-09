3ae8117e861ae147fb4912f4fc850c417dbd81cb
junee455 <michaelma455@gmail.com>
2019-06-09 03:12:20 +0300

    fixed file structure and main page

--- front/src/app/heroes/hero.ts
+++
@@ -1,9 +0,0 @@
-export class Hero {
-    id: number;
-    name: string;
-    constructor(val: Hero) {
-        // this = new Object.assign({}, val);
-        // this.id = val.id;
-        // this.name = val.name;
-    }
-}
